d0bb57f9bf445743dfb3ebb18804074a2bab11cd
Hannibal730 <cds730@naver.com>
2026-06-07 14:02:21 +0900

    🚑 fix: 휠 엔코더 핀 dmc-16 직결 버전 코드

--- arduino/wheel_encoder_test/wheel_encoder_test.ino
+++ arduino/wheel_encoder_test/wheel_encoder_test.ino
@@ -1,27 +1,39 @@
-// 엔코더 검정 → Arduino GND
-// 엔코더 빨강 → Arduino 5V
-// 엔코더 주황 → Arduino 18
-// 엔코더 갈색 → Arduino 19
-
 #define ENCODER_A 18
 #define ENCODER_B 19
 
 volatile long encoderCount = 0;
+volatile byte lastAB = 0;
 
-void encoderISR_A() {
-  if (digitalRead(ENCODER_A) == digitalRead(ENCODER_B)) {
-    encoderCount++;
-  } else {
-    encoderCount--;
-  }
-}
+void updateEncoder() {
+  byte A = digitalRead(ENCODER_A);
+  byte B = digitalRead(ENCODER_B);
 
-void encoderISR_B() {
-  if (digitalRead(ENCODER_A) != digitalRead(ENCODER_B)) {
-    encoderCount++;
-  } else {
-    encoderCount--;
+  byte currentAB = (A << 1) | B;
+  byte transition = (lastAB << 2) | currentAB;
+
+  // Quadrature encoder transition table
+  // 방향이 반대로 나오면 +1, -1을 서로 바꾸면 됨
+  switch (transition) {
+    case 0b0001:
+    case 0b0111:
+    case 0b1110:
+    case 0b1000:
+      encoderCount++;
+      break;
+
+    case 0b0010:
+    case 0b1011:
+    case 0b1101:
+    case 0b0100:
+      encoderCount--;
+      break;
+
+    default:
+      // 잘못된 전이 또는 노이즈는 무시
+      break;
   }
+
+  lastAB = currentAB;
 }
 
 void setup() {
@@ -30,21 +42,39 @@ void setup() {
   pinMode(ENCODER_A, INPUT_PULLUP);
   pinMode(ENCODER_B, INPUT_PULLUP);
 
-  attachInterrupt(digitalPinToInterrupt(ENCODER_A), encoderISR_A, CHANGE);
-  attachInterrupt(digitalPinToInterrupt(ENCODER_B), encoderISR_B, CHANGE);
+  byte A = digitalRead(ENCODER_A);
+  byte B = digitalRead(ENCODER_B);
+  lastAB = (A << 1) | B;
 
-  Serial.println("Encoder test start");
+  attachInterrupt(digitalPinToInterrupt(ENCODER_A), updateEncoder, CHANGE);
+  attachInterrupt(digitalPinToInterrupt(ENCODER_B), updateEncoder, CHANGE);
+
+  Serial.println("DMC-16 Encoder test start");
 }
 
 void loop() {
-  long nowCount;
+  static unsigned long lastMs = 0;
+  static long lastCount = 0;
 
-  noInterrupts();
-  nowCount = encoderCount;
-  interrupts();
+  if (millis() - lastMs >= 100) {
+    long nowCount;
 
-  Serial.print("ENC: ");
-  Serial.println(nowCount);
+    noInterrupts();
+    nowCount = encoderCount;
+    interrupts();
 
-  delay(50);
+    long dCount = nowCount - lastCount;
+
+    Serial.print("ENC: ");
+    Serial.print(nowCount);
+    Serial.print(" | dCount/100ms: ");
+    Serial.print(dCount);
+    Serial.print(" | A: ");
+    Serial.print(digitalRead(ENCODER_A));
+    Serial.print(" | B: ");
+    Serial.println(digitalRead(ENCODER_B));
+
+    lastCount = nowCount;
+    lastMs = millis();
+  }
 }
